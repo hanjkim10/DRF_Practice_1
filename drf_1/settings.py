d3709c36ce654ff7b0aa242b342c14ae7d26edf4
hanjkim10 <hanj.kim.10@gmail.com>
2021-10-06 16:40:17 +0900

    fix: secret key

--- drf_1/settings.py
+++ drf_1/settings.py
@@ -1,21 +1,21 @@
 from pathlib import Path
 from datetime import timedelta
+from drf_1.my_settings import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5#q2*-+!hwmoc8o-n+yuqqzn&w71#o+qtc*e-&zc_%ft9e4coe'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
